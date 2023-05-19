3b8ab4a573515fddbec4e556913a762a93488ab8
GameOneYT <71568087+GameOneYT@users.noreply.github.com>
2023-05-19 11:42:56 +0200

    Update MumblePHBot.py
    Small update,
    write to chat "? MESSAGE" to use it

--- MumblePHBot.py
+++ MumblePHBot.py
@@ -1,8 +1,10 @@
+import re
 from pornhub_api import PornhubApi
 import pymumble_py3 as pymumble
 from pymumble_py3.callbacks import PYMUMBLE_CLBK_TEXTMESSAGERECEIVED as text_received
 import requests
 import base64
+import time
 
 api = PornhubApi() 
 client = pymumble.Mumble("localhost", "PornHubBot", 64738)
@@ -12,25 +14,47 @@ client.start()
 client.is_ready()
 
     
+def message_received(message):
+    raw_message = message.message.strip()
+    if message.actor == 0:
+        # Some server will send a welcome message to the bot once connected.
+        # It doesn't have a valid "actor". Simply ignore it here.
+        return
     
-def testt(message):
+    if len(message.message) > 1 and message.message[:1] == "?":
+        send_back(message, raw_message)
+    
+    
+def send_back(message, raw_message):
+    message.message = re.sub(r'<.*?>', '', raw_message[1:])
+
+    
+    user = client.users[message.actor]['name']
+    
+    
+    print("From mumble("+user+"): "+message.message)
+    print("all: "+str(message))
     # Echo the message back to the chat
-    client.channels.find_by_name(client.my_channel()["name"]).send_text_message(str(message.message))
-    print("From mumble:")
-    print(message.message)
+    client.users[message.actor].send_text_message("Searching for: " + str(message.message))
+    
     
     # Search Pornhub for videos based on the message
-    data = api.search.search(
-        message.message,
-        ordering="mostviewed",
-        period="weekly"
-    )
+    try:
+        data = api.search.search(
+            message.message,
+            ordering="mostrelevant"#,
+            # period="weekly"
+        )
+    except:
+        client.users[message.actor].send_text_message(str("Error to find video"))
+        return
 
     # Send a list of video titles and links to the chat server
     msg_tg = ""
     videos = data.videos[:3] #3 is how many video send to the mumble
     for vid in videos:
-        print(vid.title, vid.url)
+        print(vid.title, " --- ", vid.url)
+        
         
         
         
@@ -42,11 +66,13 @@ def testt(message):
         msg_tg += str('<br /><a href="' + vid.url + '">')
         msg_tg += f'<img src="data:image/jpeg;base64,{image_b64}"/><span style="color:#39a5dd">'
         msg_tg += vid.title+"</span></a><br />"
-        client.channels.find_by_name(client.my_channel()["name"]).send_text_message(str(msg_tg))
+        #client.channels.find_by_name(client.my_channel()["name"]).send_text_message(str(msg_tg))
+        client.users[message.actor].send_text_message(str(msg_tg))
+        # client.users[message.actor].send_text_message(str("ты лох я не шучу"))
         msg_tg = ""
-    
-
+    print("\n\n")
                     
         
 while True:        
-    client.callbacks.set_callback(text_received, testt)
+    client.callbacks.set_callback(text_received, message_received)
+    time.sleep(10)
